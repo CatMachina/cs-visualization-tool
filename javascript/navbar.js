f86bcebba6c1d6636f6f800f4137fa06f7621470
Ryan Zhang <ryanse2000@gmail.com>
2022-11-26 20:56:14 -0500

    Navigation fixes again again (tentative)

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
     $.get("../navbar.html", function (data) {
-        $("#navbar-location").html(data);
-
+        $("#navbar-location-dsaav").html(data);
         $("#visualization-tool-home, #home-navbar-button").click(function() {
             const urlBreadcrumbs = window.location.href.split("/");
             urlBreadcrumbs.pop();
+            console.log(window.location.href);
             window.location.href = urlBreadcrumbs.join("/");
         });
     });
